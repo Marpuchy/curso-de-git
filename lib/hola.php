e4283bdb8f16ced805ad77270b93104a5fba3e9a
Marc Ibañez <mibanezp0709@gmail.com>
2024-09-23 12:00:40 +0200

    ahora si usa HolaMundo rebase

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,8 @@
 <?php
-// Autor: Marc  <marc@uco.es>
+// Autor: Marc Ibañez  <marc@uco.es>
+require('HolaMundo.php');
+
 print "Introduce tu nombre:";
 $nombre = trim(fgets(STDIN));
-@print "Hola, {$nombre}\n";
+print new HolaMundo($nombre);
 ?>
